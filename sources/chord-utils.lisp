0ed0deb8daf822f47f5001a3d660397985493b98
David Stephen Grant <david@davidgrant.no>
2018-10-12 22:59:02 +0200

    Fixed missing package scope resolutions.

--- sources/chord-utils.lisp
+++ sources/chord-utils.lisp
@@ -22,8 +22,8 @@
             :icon 700
   (let* ((chords-and-onsets (loop for chord in (inside self)
                                   for onset in (LOnset self)
-                                  collect (split-chords chord) into chords
-                                  collect (repeat-n onset(length (split-chords chord))) into onsets
+                                  collect (dsg::split-chords chord) into chords
+                                  collect (repeat-n onset(length (dsg::split-chords chord))) into onsets
                                   finally return (list (flat chords) (flat onsets))))
          (c-seq (objfromobjs (car chords-and-onsets) (mki 'chord-seq))))
     (setf (LOnset c-seq) (cadr chords-and-onsets))
@@ -36,7 +36,7 @@
         collect (objfromobjs note (mki 'chord))))
 
 (defmethod dsg::remove-unison-roots ((self chord))
-  (objfromobjs (remove-unison-roots (inside self)) self))
+  (objfromobjs (dsg::remove-unison-roots (inside self)) self))
 
 (defmethod dsg::remove-unison-roots ((self list)) ;; root assumed to be first note in chord <self>
   (cond ((null self) nil)
@@ -49,33 +49,33 @@
 
 (defmethod! dsg::remove-unisons ((self chord-seq))
             :icon 700
-  (let* ((clone (clone (realize-offsets self)))
+  (let* ((clone (clone (dsg::realize-offsets self)))
          (onsets (LOnset clone)))
-    (setf clone (objfromobjs (mapcar #'remove-unisons (inside clone)) clone))
+    (setf clone (objfromobjs (mapcar #'dsg::remove-unisons (inside clone)) clone))
     (setf (LOnset clone) onsets) ; Restore onsets
     clone))
 
 (defmethod! dsg::remove-unisons ((self chord))
-  (objfromobjs (remove-unisons (inside self)) self))
+  (objfromobjs (dsg::remove-unisons (inside self)) self))
 
 (defmethod! dsg::remove-unisons ((self list))
   (cond ((null self) nil)
-        (t (let ((this-note-list (remove-unison-roots self)))
-             (cons (car this-note-list) (remove-unisons (cdr this-note-list)))))))
+        (t (let ((this-note-list (dsg::remove-unison-roots self)))
+             (cons (car this-note-list) (dsg::remove-unisons (cdr this-note-list)))))))
 
 (defmethod! dsg::realize-offsets ((self chord-seq) &optional (compensate-dur-p t))
             :icon 700
-  (let ((clone (clone (split-chords self))))
+  (let ((clone (clone (dsg::split-chords self))))
     (cond (compensate-dur-p
            (setf clone (loop for chord in (inside clone)
                              ;; Shorten dur by offset or
-                             ;; by (1- dur) so that notes
-                             ;; do not become shorter than
-                             ;; 1 ms.
+                             ;; by dur (whichever is shortest)
+                             ;; so that durations do not
+                             ;; become negative
                              do (setf (LDur chord)
                                       (list (- (car (LDur chord))
                                                (min (car (LOffset chord))
-                                                    (1- (car (LDur chord)))))))
+                                                    (car (LDur chord))))))
                              collect chord into chords
                              ;; Restore onsets before returning
                              finally return (setf (LOnset (objfromobjs chords clone))
